0062873fce81b040630e19651d276aac8cff3dc4
דוד אוחנה <davidmail000@gmail.com>
2018-05-21 17:24:06 +0200

    server port process.env.PORT

--- server.js
+++ server.js
@@ -68,6 +68,6 @@ io.on('connection', function(socket) {
 
 
 //--------------------------------------------------------------------
-http.listen(8888, function() {
+http.listen(process.env.PORT, function() {
     console.log('listening on *:8888');
 });
